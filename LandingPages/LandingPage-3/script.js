9e37e6ae30fb19e3fd5019646e7903ec76270da8
ALdemir Ferreira <93743515+aldemirferreira44@users.noreply.github.com>
2022-01-11 13:47:55 -0200

    .

---
+++ LandingPages/LandingPage-3/script.js
@@ -0,0 +1,7 @@
+var check = document.querySelector(".num").innerHTML;
+
+function soma() {
+    if (check == 2) { window.alert("Confirmado") }
+    else { window.alert("Por favor, tente novamente") }
+
+}
